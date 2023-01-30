c5384db5ac261a96c85e619b3f67736da95ca8c8
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-30 13:55:18 -0300

    Add Select Component Basic Structure

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,38 @@
-function Select() {
-  return <h1>Hello</h1>;
+import styled from "styled-components";
+
+type SelectOption = {
+  value: string;
+  label: string;
+};
+
+type SelectProps = {
+  value?: SelectOption;
+  onChange: (value: SelectOption | undefined) => void;
+  options: SelectOption[];
+};
+
+function Select({ value, onChange, options }: SelectProps) {
+  return (
+    <SelectContainer>
+      <span className="value">Value</span>
+      <button className="clear-btn">&times;</button>
+      <div className="divider"></div>
+      <div className="caret"></div>
+      <ul className="options">
+        {options.map((option) => (
+          <li key={option.value} className="option">
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </SelectContainer>
+  );
 }
+
 export default Select;
+
+const SelectContainer = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`;
